3ff76c62cf86210ac70e3010fef65d9b4824e25b
ayyappakumar-t <67931668+ayyappakumar-t@users.noreply.github.com>
2020-09-23 23:38:34 +0530

    Added Calculator_Interface and Console

---
+++ Project/Calculator_Interface/CalculatorAdapter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+
+namespace Calculator_Interface
+{
+    public class CalculatorAdapter : ICalculatorAdapter
+    {
+        private string apiUrl = "https://localhost:44314/api/Home/";
+        private bool _includeAPIAndDB = false;
+        public int Add(int start, int amount)
+        {
+            if (_includeAPIAndDB)
+            {
+                // Calling with API and DB
+                using (var client = new WebClient())
+                {
+                    var urlAddress = apiUrl + "Add" + "?start=" + start + "&amount=" + amount;
+                    var result = client.DownloadString(urlAddress);
+                    return !string.IsNullOrEmpty(result) ? Convert.ToInt32(result) : 0;
+                }
+            }
+            else
+            {
+                // Calling without API and DB
+                return start + amount;
+            }
+        }
+
+        public int Subtract(int start, int amount)
+        {
+            if (_includeAPIAndDB)
+            {
+                // Calling with API and DB
+                using (var client = new WebClient())
+                {
+                    var urlAddress = apiUrl + "Subtract" + "?start=" + start + "&amount=" + amount;
+                    var result = client.DownloadString(urlAddress);
+                    return !string.IsNullOrEmpty(result) ? Convert.ToInt32(result) : 0;
+                }
+            }
+            else
+            {
+                // Calling without API and DB
+                return start - amount;
+            }
+        }
+
+        public int Divide(int start, int by)
+        {
+            if (_includeAPIAndDB)
+            {
+                // Calling with API and DB
+                using (var client = new WebClient())
+                {
+                    var urlAddress = apiUrl + "Divide" + "?start=" + start + "&by=" + by;
+                    var result = client.DownloadString(urlAddress);
+                    return !string.IsNullOrEmpty(result) ? Convert.ToInt32(result) : 0;
+                }
+            }
+            else
+            {
+                // Calling without API and DB
+                return start / by;
+            }
+        }
+
+        public int Multiply(int start, int by)
+        {
+            if (_includeAPIAndDB)
+            {
+                // Calling with API and DB
+                using (var client = new WebClient())
+                {
+                    var urlAddress = apiUrl + "Multiply" + "?start=" + start + "&by=" + by;
+                    var result = client.DownloadString(urlAddress);
+                    return !string.IsNullOrEmpty(result) ? Convert.ToInt32(result) : 0;
+                }
+            }
+            else
+            {
+                // Calling without API and DB
+                return start * by;
+            }
+        }
+    }
+}
---
+++ Project/Project/CalculatorInject.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator_Interface;
+
+namespace Calculator
+{
+    class CalculatorInject
+    {
+        private ICalculatorAdapter _Calculator;
+        public CalculatorInject(ICalculatorAdapter _Calculator)
+        {
+            this._Calculator = _Calculator;
+        }
+        public int Add(int start, int amount)
+        {
+            return _Calculator.Add(start, amount);
+        }
+
+        public int Divide(int start, int by)
+        {
+            return _Calculator.Divide(start, by);
+        }
+
+        public int Multiply(int start, int by)
+        {
+            return _Calculator.Multiply(start, by);
+        }
+
+        public int Subtract(int start, int amount)
+        {
+            return _Calculator.Subtract(start, amount);
+        }
+    }
+}
